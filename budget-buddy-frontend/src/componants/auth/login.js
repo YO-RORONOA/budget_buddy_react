c073e2d147ea843c6449f2c17f4ba1208caabf66
youness <youness.ouahmane4@gmail.com>
2025-04-10 21:49:46 +0100

    Handle and display login errors with conditional message rendering

--- budget-buddy-frontend/src/componants/auth/login.js
+++ budget-buddy-frontend/src/componants/auth/login.js
@@ -11,4 +11,19 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     setMessage('');
-   
+    
+    try {
+      await AuthService.login(email, password);
+      navigate('/');
+      window.location.reload();
+    } catch (error) {
+      const resMessage =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      setMessage(resMessage);
+    }
+  };
+
